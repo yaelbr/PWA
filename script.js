7b3b8e6c821890cb96bde5ffd2c3d9bcd9175485
yael.b <yael.s@weunica.com>
2024-01-18 12:04:04 +0200

    commit with a instead iframe

--- script.js
+++ script.js
@@ -10,24 +10,24 @@ if ('serviceWorker' in navigator) {
             });
     });
 }
-document.addEventListener('DOMContentLoaded', () => {
-    const iframe = document.getElementById('browser-container');
-    function loadWebsite(url) {
-        // const iframe = document.createElement('iframe');
-        iframe.src = url;
-        iframe.setAttribute('allow', 'fullscreen');
-        iframe.addEventListener('load', () => {
-            // Enable scrolling within the iframe
-              iframe.contentWindow.document.body.style.overflow = 'auto';
-            // const iframeDoc = iframe.contentDocument || iframe.contentWindow.document;
-            // iframeDoc.documentElement.style.overflow = 'hidden';
-            // iframeDoc.body.style.overflow = 'hidden';
-        });
-        // browserContainer.innerHTML = '';
-        //browserContainer.appendChild(iframe);
-    }
-    // Example usage:
-    loadWebsite('https://yaels936.wixsite.com/post');
-});
+// document.addEventListener('DOMContentLoaded', () => {
+//     const iframe = document.getElementById('browser-container');
+//     function loadWebsite(url) {
+//         // const iframe = document.createElement('iframe');
+//         iframe.src = url;
+//         iframe.setAttribute('allow', 'fullscreen');
+//         iframe.addEventListener('load', () => {
+//             // Enable scrolling within the iframe
+//               iframe.contentWindow.document.body.style.overflow = 'auto';
+//             // const iframeDoc = iframe.contentDocument || iframe.contentWindow.document;
+//             // iframeDoc.documentElement.style.overflow = 'hidden';
+//             // iframeDoc.body.style.overflow = 'hidden';
+//         });
+//         // browserContainer.innerHTML = '';
+//         //browserContainer.appendChild(iframe);
+//     }
+//     // Example usage:
+//     loadWebsite('https://yaels936.wixsite.com/post');
+// });
 
 
