cb87e874d357f801733b7fc15ab458bd8da989f2
yael.b <yael.s@weunica.com>
2024-01-18 10:49:06 +0200

    Initial commit

---
+++ sw.js
@@ -0,0 +1,23 @@
+self.addEventListener('install', event => {
+    event.waitUntil(
+      caches.open('your-website-cache')
+        .then(cache => {
+          return cache.addAll([
+            '/',
+            '/index.html',
+            '/styles.css',
+            '/script.js',
+          ]);
+        })
+    );
+  });
+  
+  self.addEventListener('fetch', event => {
+    event.respondWith(
+      caches.match(event.request)
+        .then(response => {
+          return response || fetch(event.request);
+        })
+    );
+  });
+  
