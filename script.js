75f8a932f83e76cb5e1af3bfdb9f22ed47ad7f87
yael.b <yael.s@weunica.com>
2024-01-18 12:08:06 +0200

    commit without "

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 if ('serviceWorker' in navigator) {
     console.log('in');
     window.addEventListener('load', () => {
-        navigator.serviceWorker.register('/sw.js')
+        navigator.serviceWorker.register('sw.js')
             .then(registration => {
                 console.log('Service Worker registered: ', registration);
             })
