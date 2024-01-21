8b9e49964babce0db949889e1f725ef0cd71073d
yael.b <yael.s@weunica.com>
2024-01-21 15:42:13 +0200

    commit see fetch

--- sw.js
+++ sw.js
@@ -1,25 +1,24 @@
 self.addEventListener('install', event => {
-    event.waitUntil(
-      caches.open('your-website-cache')
-        .then(cache => {
-          return cache.addAll([
-            '',
-            'index.html',
-            'styles.css',
-            'script.js',
-            
-          ]);
-        })
-    );
-  });
-  
-  self.addEventListener('fetch', event => {
-    console.log('in');
-    event.respondWith(
-      caches.match(event.request)
-        .then(response => {
-          return response || fetch(event.request);
-        })
-    );
-  });
-  
+  event.waitUntil(
+    caches.open('your-website-cache')
+      .then(cache => {
+        return cache.addAll([
+          '',
+          'index.html',
+          'styles.css',
+          'script.js',
+        ]);
+      })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  console.log('in');
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        console.log('fetch', response);
+        return response || fetch(event.request);
+      })
+  );
+});
