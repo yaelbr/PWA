3bbf94d24f2a0bcd47434560af37af64ae44335f
yael.b <yael.s@weunica.com>
2024-01-21 11:11:11 +0200

    commit 3

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
         navigator.serviceWorker.register('sw.js')
             .then(registration => {
-                console.log('Service Worker registered: ', registration);
+                console.log('Service Worker registered good: ', registration);
             })
             .catch(error => {
                 console.log('Service Worker registration failed: ', error);
