ef4e6e0785972e7a822774201013e96f4c7e67c1
yael.b <yael.s@weunica.com>
2024-01-22 15:42:07 +0200

    commit back

--- sw.js
+++ sw.js
@@ -1,19 +1,15 @@
 const cacheName = 'your-website-cache-v1'; // Update the cache version
 
-const cachedAssets = [
-  '/',
-  'index.html',
-  'styles.css',
-  'script.js',
-  'icons/icon.png',
-  // Add more paths to important assets, such as images, fonts, etc.
-];
-
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(cacheName)
       .then(cache => {
-        return cache.addAll(cachedAssets);
+        return cache.addAll([
+          '',
+          'index.html',
+          'styles.css',
+          'script.js'
+        ]);
       })
   );
 });
@@ -34,29 +30,24 @@ self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request)
       .then(response => {
-        return response || fetchAndCache(event.request);
+        if (response) {
+          return response;
+        }
+
+        return fetch(event.request).then(response => {
+          if (!response || response.status !== 200 || response.type !== 'basic') {
+            return response;
+          }
+
+          const responseToCache = response.clone();
+
+          caches.open(cacheName)
+            .then(cache => {
+              cache.put(event.request, responseToCache);
+            });
+
+          return response;
+        });
       })
   );
 });
-
-function fetchAndCache(request) {
-  return fetch(request)
-    .then(response => {
-      // Check if we received a valid response
-      if (!response || response.status !== 200 || response.type !== 'basic') {
-        return response;
-      }
-
-      const responseToCache = response.clone();
-
-      caches.open(cacheName)
-        .then(cache => {
-          cache.put(request, responseToCache);
-        });
-
-      return response;
-    })
-    .catch(error => {
-      console.error('Error fetching and caching:', error);
-    });
-}
