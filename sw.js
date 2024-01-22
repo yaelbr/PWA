db1780a608b9ccfdf08095ea9e9754cfe5e9f0f5
yael.b <yael.s@weunica.com>
2024-01-22 11:03:29 +0200

    commit delete button

--- sw.js
+++ sw.js
@@ -51,29 +51,29 @@ self.addEventListener('fetch', event => {
       })
   );
 });
-let deferredPrompt;
+// let deferredPrompt;
 
-window.addEventListener('beforeinstallprompt', (e) => {
-  // Prevent the mini-infobar from appearing on mobile
-  e.preventDefault();
-  // Stash the event so it can be triggered later.
-  deferredPrompt = e;
-  // Update UI notify the user they can install the PWA
-  showInstallPromotion();
-  // Optionally, send analytics event that PWA install promo was shown.
-  console.log(`'beforeinstallprompt' event was fired.`);
-});
-let buttonInstall = document.getElementsByClassName('button');
-console.log('buttonnn');
-buttonInstall.addEventListener('click', async () => {
-  // Hide the app provided install promotion
-  hideInstallPromotion();
-  // Show the install prompt
-  deferredPrompt.prompt();
-  // Wait for the user to respond to the prompt
-  const { outcome } = await deferredPrompt.userChoice;
-  // Optionally, send analytics event with outcome of user choice
-  console.log(`User response to the install prompt: ${outcome}`);
-  // We've used the prompt, and can't use it again, throw it away
-  deferredPrompt = null;
-});
+// window.addEventListener('beforeinstallprompt', (e) => {
+//   // Prevent the mini-infobar from appearing on mobile
+//   e.preventDefault();
+//   // Stash the event so it can be triggered later.
+//   deferredPrompt = e;
+//   // Update UI notify the user they can install the PWA
+//   showInstallPromotion();
+//   // Optionally, send analytics event that PWA install promo was shown.
+//   console.log(`'beforeinstallprompt' event was fired.`);
+// });
+// let buttonInstall = document.getElementsByClassName('button');
+// console.log('buttonnn');
+// buttonInstall.addEventListener('click', async () => {
+//   // Hide the app provided install promotion
+//   hideInstallPromotion();
+//   // Show the install prompt
+//   deferredPrompt.prompt();
+//   // Wait for the user to respond to the prompt
+//   const { outcome } = await deferredPrompt.userChoice;
+//   // Optionally, send analytics event with outcome of user choice
+//   console.log(`User response to the install prompt: ${outcome}`);
+//   // We've used the prompt, and can't use it again, throw it away
+//   deferredPrompt = null;
+// });
