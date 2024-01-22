d80d89e07d97d51db63d9a93229c85568cf7c711
yael.b <yael.s@weunica.com>
2024-01-22 10:31:18 +0200

    commit try another

--- sw.js
+++ sw.js
@@ -1,5 +1,6 @@
+const cacheName = 'your-website-cache-v1'; // Update the cache version
+
 self.addEventListener('install', event => {
-  const cacheName = 'your-website-cache-v1'; // Update the cache version
   event.waitUntil(
     caches.open(cacheName)
       .then(cache => {
@@ -12,11 +13,12 @@ self.addEventListener('install', event => {
       })
   );
 });
+
 self.addEventListener('activate', event => {
   event.waitUntil(
     caches.keys().then(keys => {
       return Promise.all(keys.map(key => {
-        if (key !== cacheName) { // Update cacheName to your new version
+        if (key !== cacheName) {
           return caches.delete(key);
         }
       }));
@@ -28,20 +30,24 @@ self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request)
       .then(response => {
-        // If the response is in the cache, return it
         if (response) {
           return response;
         }
 
-        // If the response is not in the cache, fetch it from the network
         return fetch(event.request).then(response => {
-          // Update the cache with the latest version
-          return caches.open(cacheName).then(cache => {
-            cache.put(event.request, response.clone());
+          if (!response || response.status !== 200 || response.type !== 'basic') {
             return response;
-          });
+          }
+
+          const responseToCache = response.clone();
+
+          caches.open(cacheName)
+            .then(cache => {
+              cache.put(event.request, responseToCache);
+            });
+
+          return response;
         });
       })
   );
 });
-
